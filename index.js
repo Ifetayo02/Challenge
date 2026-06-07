17ef1bf09cd97e4c79f6a6a00b9c0ae0c5ebb222
Ifetayo02 <abdulqoyumjamiu@gmail.com>
2026-06-07 16:42:49 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const http=require('http')
+const server= http.createServer((req,res)=>{
+    if (req.method ==='GET' && req.url=='/'){
+        res.writeHead(200,{'Content-Type':'application/json'})
+        res.end(JSON.stringify({message:'Hello World'}))
+}else{
+    res.writeHead(404,{'Content-Type':'application/json'})
+    res.end(JSON.stringify({message:'Not Found'}))
+}
+});
+server.listen(3000,()=>{
+    console.log('Server is running on port 3000')
+})
