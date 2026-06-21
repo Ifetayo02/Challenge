c14eb5ab7e953c6ed051b19f63baf72121010839
Ifetayo02 <abdulqoyumjamiu@gmail.com>
2026-06-21 19:20:41 +0100

    jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,23 +1,63 @@
-const User=require('../models/userModel');
-const registerUser=async(req,res,next)=>{
-    try{
-    const{name,email,password}=req.body;
-    const userExists= await User.findOne({email});
-    if (userExists){
-        const error=new Error ('User with this email already exists');
-        error.statusCode=400;
-        return next(error)
-    }
-const newUser=await User.create({name,email,password});
-const userResponse=new User.toObject();
-delete userResponse.password;
-res.status(201).json({
+const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const registerUser = async (req, res, next) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const userExists = await User.findOne({ email: email.toLowerCase() });
+        if (userExists) {
+            const error = new Error('User with this email already exists');
+            error.statusCode = 400;
+            return next(error);
+        }
+        const newUser = await User.create({ name, email, password });
+
+        const userResponse = newUser.toObject();
+        delete userResponse.password;
+
+        return res.status(201).json({
             success: true,
             data: userResponse
-        })
-    }catch (error) {
-          if (!error.statusCode) error.statusCode = 400
+        });
+    } catch (error) {
+        error.statusCode = 400;
         next(error);
     }
-}
-module.exports = { registerUser };
+};
+const loginUser = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            const error = new Error('Please provide email and password');
+            error.statusCode = 400;
+            return next(error);
+        }
+        const user = await User.findOne({ email: email.toLowerCase() });
+        if (!user) {
+            const error = new Error('Invalid email or password');
+            error.statusCode = 401;
+            return next(error);
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            const error = new Error('Invalid email or password');
+            error.statusCode = 401;
+            return next(error);
+        }
+        const token = jwt.sign(
+            { id: user._id, email: user.email },
+            process.env.JWT_SECRET,
+            { expiresIn: '30d' }
+        );
+
+        return res.status(200).json({
+            success: true,
+            token: token
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+module.exports = { registerUser, loginUser };
--- models/userModel.js
+++ models/userModel.js
@@ -22,14 +22,11 @@ const userSchema = new mongoose.Schema({
     timestamps: true
 }
 );
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password'));
-    try {
-        const salt = await bcrypt.genSalt(12);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (error) {
-        next(error);
-    }
+userSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
+
+    const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, salt);
 });
+
 module.exports = mongoose.model('User', userSchema);
